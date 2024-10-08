6a4463163276872796efcb884f15d12165ad8ff0
NovaChemcorp <fernandblucie26@gmail.com>
2024-10-08 16:29:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById('buyButton').addEventListener('click', function() {
     window.location.href = "https://mi-tienda.com/checkout";
 });
+background: radial-gradient(circle, #ff7e5f, #feb47b);
